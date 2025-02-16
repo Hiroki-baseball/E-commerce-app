891b11b9f21c673695a31302e997dbac64722b7a
HirokiNakano <baseballinobu@gmail.com>
2025-02-17 00:37:24 +0900

    モーダル修正

--- src/app/components/Taco.tsx
+++ src/app/components/Taco.tsx
@@ -102,7 +102,7 @@ const Taco = ({ taco, isPurchased, user }: Props) => {
             </a>
 
             {showModal && (
-              <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
+              <div className="fixed top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal z-50">
                 <div className="bg-white p-8 rounded-lg">
                   <h3 className="text-xl mb-4">レシピを購入しますか？</h3>
                   <button
